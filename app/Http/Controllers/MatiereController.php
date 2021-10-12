e6b25d856e8497b004560d983a6e016a22c57877
Ezéchias W Kouda <ekouda006@gmail.com>
2021-10-12 14:09:05 +0000

    adding of filiere and matiere migrations

---
+++ app/Http/Controllers/MatiereController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Filiere;
+use App\Models\Matiere;
+use Illuminate\Http\Request;
+
+class MatiereController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $filiere = Filiere::all();
+        return view("filieres")->with('filieres',$filiere);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Parametre/Filiere/add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Matiere  $matiere
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Matiere $matiere)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Matiere  $matiere
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Matiere $matiere)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Matiere  $matiere
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Matiere $matiere)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Matiere  $matiere
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Matiere $matiere)
+    {
+        //
+    }
+}
