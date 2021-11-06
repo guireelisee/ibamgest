fa59bde8d7e9e314f4959bb1d3f741e371504bc7
guireelisee <guireelisee@gmail.com>
2021-11-06 01:36:58 +0000

    add google login 1

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 
 class SocialiteController extends Controller
